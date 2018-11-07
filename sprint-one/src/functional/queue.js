91f3830c8d0e4913693238ffbe602ac002fe4bbc
Kevin Li <li.ctrl.z@gmail.com>
2018-11-07 12:05:45 -0800

    Implemented queue with specrunner tests passing

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,18 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[value] = value;
   };
 
   someInstance.dequeue = function() {
+    let keyArr = Object.keys(storage);
+    let firstKeyArr = keyArr[0];
+    delete storage[firstKeyArr];
+    return firstKeyArr;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
