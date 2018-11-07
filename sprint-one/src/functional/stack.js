9f41cf92608e26058a8cf47cd3d2606f98519488
Kevin Li <li.ctrl.z@gmail.com>
2018-11-07 12:02:33 -0800

    Implemented stack with specrunner tests passing

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,21 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[value] = value;
   };
 
   someInstance.pop = function() {
+    let keyArr = Object.keys(storage);
+    let lastKeyArr = keyArr[keyArr.length - 1];
+    delete storage[lastKeyArr];
+    return lastKeyArr;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
+
   };
 
   return someInstance;
